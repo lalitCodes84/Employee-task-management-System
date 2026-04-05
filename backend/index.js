9f32b00388cf04135d465e2df384518e664f0ed6
Anurag <anurag@gmail.com>
2026-04-05 22:31:23 +0530

    Initial Setup Commit

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const cors = require("cors");
+ const PORT = 4444;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res) => {
+    try {
+        res.send(`Server Started Successfully`)
+    } catch (error) {
+        res.send(`Server Error`)
+    }
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server Started Successfully On Port ${PORT}`)
+})
+
+
+
