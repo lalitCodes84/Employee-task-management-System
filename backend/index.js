1aa8f828ad5bc52264ec9b229ffe4e196ed5576b
Anurag <anurag@gmail.com>
2026-04-05 23:56:26 +0530

    Connected MYSql DB

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cors = require("cors");
  const PORT = 4444;
+ const db = require("./config/db");
+
+ 
 const app = express();
 app.use(cors());
 app.use(express.json());
